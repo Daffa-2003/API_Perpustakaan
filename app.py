e732d6ef0fcf7850e430901f5fbccd6b7d8652b9
Farih65 <124002868+Farih65@users.noreply.github.com>
2024-12-03 13:47:04 +0700

    Update app.py
    change database url for cloud db

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ import datetime
 app = Flask(__name__)
 CORS(app)
 # url = 'postgresql://postgres:otobook@10.0.2.37:5432/perpustakaan'
-url = 'postgresql://postgres:daffa@localhost/Perpustakaan'
+# url = 'postgresql://postgres:daffa@localhost/Perpustakaan'
+url = 'postgresql://postgres:cfsfMm58kxjpWDfMZUR5@digitallibrary-db.cjk04w8y2xry.ap-southeast-3.rds.amazonaws.com/Perpustakaan'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 db = SQLAlchemy(app)
