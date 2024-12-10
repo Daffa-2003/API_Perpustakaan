b3a28258a19b3819f112f23a122088beeb69a9a3
Farih65 <124002868+Farih65@users.noreply.github.com>
2024-12-10 11:08:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ app = Flask(__name__)
 CORS(app)
 # url = 'postgresql://postgres:otobook@10.0.2.37:5432/perpustakaan'
 # url = 'postgresql://postgres:daffa@localhost/Perpustakaan'
-url = 'postgresql://postgres:cfsfMm58kxjpWDfMZUR5@digitallibrary-db.cjk04w8y2xry.ap-southeast-3.rds.amazonaws.com:5432/perpustakaan'
+#url = 'postgresql://postgres:cfsfMm58kxjpWDfMZUR5@digitallibrary-db.cjk04w8y2xry.ap-southeast-3.rds.amazonaws.com:5432/perpustakaan'
+url = 'postgresql://postgres:cfsfMm58kxjpWDfMZUR5@dl-public.cjk04w8y2xry.ap-southeast-3.rds.amazonaws.com:5432/perpustakaan'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = url
 db = SQLAlchemy(app)
